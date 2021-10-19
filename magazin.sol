908a10abf49f6844795b99eb9e8019ad541c2de6
lombrozomir <92676423+lombrozomir@users.noreply.github.com>
2021-10-19 19:20:27 +0300

    Update magazin.sol

--- magazin.sol
+++ magazin.sol
@@ -16,14 +16,11 @@ contract magazin {
 
 
     constructor() public {
-        // Check that contract's public key is set
+
         require(tvm.pubkey() != 0, 101);
-        // Check that message has signature (msg.pubkey() is not zero) and
-        // message is signed with the owner's private key
+
         require(msg.pubkey() == tvm.pubkey(), 102);
-        // The current smart contract agrees to buy some gas to finish the
-        // current transaction. This actions required to process external
-        // messages, which bring no value (henceno gas) with themselves.
+
         tvm.accept();
         timestamp = now;
     }
@@ -35,17 +32,13 @@ contract magazin {
 		_;
 	}
 
-    function deleteOchered() public checkOwnerAndAccept returns(string[]) {
+    function deleteOchered() public checkOwnerAndAccept {
         
-        uint count = myArr.length;
-
-        for (uint i = 0; i < count-1; i++){
+        for (uint i = 0; i < myArr.length - 1; i++){
             myArr[i] = myArr[i+1];
         }
-        delete myArr[count-1];
-        count--;
         myArr.pop();
-        return myArr;
+
     }
 
     function addOchered(string value) public checkOwnerAndAccept{
